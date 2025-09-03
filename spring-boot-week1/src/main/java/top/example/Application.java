6489345c7192589d4829f04f373394efdb0eaa2d
Colour-Xi <1226509757@qq.com>
2025-09-03 16:21:21 +0800

    week1

---
+++ spring-boot-week1/src/main/java/top/example/Application.java
@@ -0,0 +1,23 @@
+package top.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+@RestController
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.printf("run app");
+    }
+    @GetMapping("/hello")
+    public String index() {
+        return "hello world!";
+    }
+}
---
+++ src/test/java/top/example/sbc/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package top.example.sbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
