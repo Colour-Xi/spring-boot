7ff5a08444c1e93f00006d45d84d0fc04fafa31a
Colour-Xi <1226509757@qq.com>
2025-09-03 16:21:21 +0800

    week1

--- spring-boot-week1/src/main/java/top/example/Application.java
+++ spring-boot-week1/src/main/java/top/example/Application.java
@@ -15,6 +15,7 @@ public class Application {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("run app");
+        System.out.println("run test1");
     }
     @GetMapping("/hello")
     public String index() {
